42788f98774b5dc3531b75a12ecb2a4efa23fb81
Andrés Born <andres.born@gmail.com>
2026-03-24 12:17:22 -0600

    Logica computaora

---
+++ Scenes/Computadora/TriviaPopup/question_container.gd
@@ -0,0 +1,69 @@
+extends Control
+
+@onready var preguntaLabel = %PreguntaLabel
+@onready var button = %Button
+@onready var button2 = %Button2
+@onready var button3 = %Button3
+@onready var button4 = %Button4
+
+var preguntas: Array = []
+var preguntaActual = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	preguntas = load_json()
+	set_questions(preguntas)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func load_json():
+	var file = FileAccess.open("res://Scenes/Computadora/TriviaPopup/questions.json", FileAccess.READ)
+	var json = JSON.new()
+	var result: Error = json.parse(file.get_as_text())
+	if result != OK:
+		print(json.get_error_line())
+		print(json.get_error_message())
+	else:
+		return json.data
+
+func set_questions(preguntas: Array):
+	var pregunta = preguntas.pick_random()
+	preguntaActual = pregunta
+	preguntaLabel.text = pregunta["label"]
+
+	var opciones = pregunta["opciones"]
+	button.text = opciones[0]["label"]
+	button2.text = opciones[1]["label"]
+	button3.text = opciones[2]["label"]
+	button4.text = opciones[3]["label"]
+	
+func handle_on_click(button: Button):
+	# Revisar si la opción es la correcta
+	var opciones = preguntaActual["opciones"]
+	var opcion = {}
+	for x in opciones:
+		if x.label == button.text:
+			opcion = x
+	var isCorrect = opcion["correcta"]
+	if isCorrect:
+		print("Escogiste la opción correcta")
+	else:
+		print("Te equivocaste!")
+
+
+func _on_button_pressed() -> void:
+	handle_on_click(button)
+
+
+func _on_button_2_pressed() -> void:
+	handle_on_click(button2)
+
+
+func _on_button_3_pressed() -> void:
+	handle_on_click(button3)
+
+
+func _on_button_4_pressed() -> void:
+	handle_on_click(button4)
