cb02043751cb050191089a1f04fa023cb72baadc
Daniel <danielcon23@gmail.com>
2026-03-25 01:03:39 -0600

    microjuego ya con nodos

--- Scenes/Ejemplos/sustento.gd
+++ Scenes/Ejemplos/sustento.gd
@@ -1,44 +1,90 @@
 extends Control
 #lo siguiente es para 1 de de 3 microjuegos que podríamos hacer en la ventana de sustento
 #idealmente un microjuego podr[ia mostrar m[as de una receta
-@export var tiempo_meta: float  = 5
-@export var margen_error: float = 2
+@export var tiempo_meta: float  = 10
+@export var margen_error: float = 1
+@export var tiempo_maximo: float = 15
+
+@onready var pollo = $Poio
+@onready var humo = $Humo
+@onready var sfx_cocina = $SFX_Cocina
+@onready var sfx_resultado_bien = $SFX_Resultado_Bien
+@onready var sfx_resultado_mal = $SFX_Resultado_Mal
+
+
 var tiempo_cocinado: float = 0
-
 var cocinando: bool = false
+var fin: bool = false
+
+#COLORES DE POLLO
+var color_crudo = Color(1,1,0.7)
+var color_perfecto = Color(1, 0.6, 0)
+var color_quemado = Color(0.2, 0.1, 0)
 
 
-#------------------------------------------------------------------------------------------
 
-func _process(delta: float):
-	if cocinando:
+#nada mas para que empiece en color crudo y sin humo
+func _ready():
+	pollo.modulate = color_crudo
+	humo.emitting = false
+	
+#mientras que el pollo se cocina, cambia de color y vibra un poco
+func _physics_process(delta: float):
+	if cocinando and not fin:
 		tiempo_cocinado += delta
-		#codigo de cocinando aqui
-		#animacion de cocinando aqui
+		actualizar_color_pollo()
+		#vibrar_pollo()
+		
+		if tiempo_cocinado >= tiempo_maximo:
+			termina_de_cocinar()
 
+#Mueve el color del pollo conforme se acerca al color xico y luego pasa a color quemado
+func actualizar_color_pollo():
+	if tiempo_cocinado < tiempo_meta:
+		var t = tiempo_cocinado / tiempo_meta
+		pollo.modulate = color_crudo.lerp(color_perfecto, t)
+	else:
+		var t = (tiempo_cocinado - tiempo_meta) / (tiempo_maximo - tiempo_meta)
+		pollo.modulate = color_perfecto.lerp(color_quemado, t)
+
+#Mueve un poquito el pollo, no es realista pero siento que hace la escena menos est[atica
+#lo deje apagado porque me preocupa que se rompa al integrarlo al resto del juego
+#func vibrar_pollo():
+	#pollo.position = Vector2(randf_range(-1, 1), randf_range(-1,1)) 
+
+#cocina si presiona ui_accept
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("ui_accept") and not fin:
 		if not cocinando:
 			empieza_a_cocinar()
 		else:
 			termina_de_cocinar()
-			
+
+#corre timer de cocinando, activa humo
 func empieza_a_cocinar():
 	cocinando = true
-	tiempo_cocinado = 0
+	humo.emitting = true
+	sfx_cocina.play()
 	print("cocinando...")
- 	
+	
+
+#apaga timer de cocinando
 func termina_de_cocinar():
 	cocinando = false
+	fin = true
+	sfx_cocina.stop()
 	print("Lo removiste del calor, pasaron " + str(tiempo_cocinado) + " segundos")
 	resultado()
 	
 	
 func resultado():
 	var diferencia = abs(tiempo_meta - tiempo_cocinado)
-	if diferencia <= margen_error: print("Se puede comer!")
-	#codigo de cambio de sprite va aca
-	elif tiempo_cocinado < tiempo_meta: print("IT's FUCKEN RAAAAW")
-	#codigo de comida cruda aca, sprite y afecta el main game
-	else:print("La puta madreee, se quemó.")
-	#codigo de basura quemada aca, sprite y afecta el main game 
+	if diferencia <= margen_error: 
+		print("Se puede comer!")
+		sfx_resultado_bien.play()
+	elif tiempo_cocinado < tiempo_meta:
+		print("IT's FUCKEN RAAAAW")
+		sfx_resultado_mal.play()
+	else:
+		print("La puta madreee, se quemó.") 
+		sfx_resultado_mal.play()
