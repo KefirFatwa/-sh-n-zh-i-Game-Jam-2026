210e06fc7328f7e0f2f293155baa8edfc9e54254
Andrés Born <andres.born@gmail.com>
2026-03-26 22:56:57 -0600

    Celular popups y retoques

--- Scenes/Celular/celular_scene.gd
+++ Scenes/Celular/celular_scene.gd
@@ -2,7 +2,8 @@ extends Control
 
 @onready var button = %Button
 @onready var timer = %PopUpTimer
-@onready var popUpSpawnLocation: PathFollow2D = %PopUpSpawnLocation
+@onready var screen = %Screen
+@onready var markerController = %MarkerController
 
 var popUpScene = preload("res://Scenes/Celular/PopUp/PopUp.tscn")
 
@@ -30,7 +31,19 @@ func _on_button_pressed() -> void:
 
 
 func _on_timer_timeout() -> void:
+	create_popUp()
+	
+func create_popUp() -> void:
 	var popUp: ColorRect = popUpScene.instantiate()
-	popUpSpawnLocation.progress_ratio = randf()
-	popUp.position = popUpSpawnLocation.position
-	add_child(popUp)
+	var markers = markerController.get_children()
+	var marker = markers.pick_random()
+	if marker.get_children().size() < 1:
+		marker.add_child(popUp)
+		return
+	
+	var markers_without_children = markers.filter(func (m): 
+		m.get_children().size() < 1)
+	if markers_without_children.size() > 0:
+		markers_without_children.pick_random().add_child(popUp)
+		return
+	print("No mas espacio para popups.")
