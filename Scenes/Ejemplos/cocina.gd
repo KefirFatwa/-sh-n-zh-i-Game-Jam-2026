f636d5918765522a1593d5f65d452cab7cc23c10
Daniel <danielcon23@gmail.com>
2026-03-28 15:17:10 -0600

    Labels cocina reciben signal de GM

--- Scenes/Ejemplos/cocina.gd
+++ Scenes/Ejemplos/cocina.gd
@@ -5,27 +5,46 @@ class_name CocinaManager
 @export var minijuego : Array[PackedScene]= []
 var hay_juego_activo : bool = false
 
-@onready var chicken_button: Button = %Chicken_button
-@onready var noddle_button: Button = %noddle_button
-@onready var tacos_button: Button = %Tacos_button
+@onready var chicken_button: Button = %chicken_button
+@onready var noodle_button: Button = %noddle_button
+@onready var tacos_button: Button = %tacos_button
 
 
 @onready var cocina: CocinaManager = %Cocina
 
 func _ready() -> void:
 	GameManager.pollo_actualizado.connect(_actualizar_texto_cantidad_pollo)
+	GameManager.maruchan_actualizado.connect(_actualizar_texto_cantidad_noodle)
+	GameManager.taco_actualizado.connect(_actualizar_texto_cantidad_tacos)
+	
+	_actualizar_texto_cantidad_pollo(GameManager.pollo_cantidad)
+	_actualizar_texto_cantidad_noodle(GameManager.maruchan_cantidad)
+	_actualizar_texto_cantidad_tacos(GameManager.taco_cantidad)
 
-func _actualizar_texto_cantidad_pollo(cantidad_pollo)->void:
-	chicken_button.text = "x" + str(cantidad_pollo)
+func _actualizar_texto_cantidad_pollo(cantidad)->void:
+	chicken_button.text = "x" + str(cantidad)
+func _actualizar_texto_cantidad_noodle(cantidad)->void:
+	noodle_button.text = "x" + str(cantidad)
+func _actualizar_texto_cantidad_tacos(cantidad)->void:
+	tacos_button.text = "x" + str(cantidad)
 
 func _on_chicken_button_pressed() -> void:
-	_spawn_minigame(minijuego[0])
+	if GameManager.pollo_cantidad > 0:
+		_spawn_minigame(minijuego[0])
+		GameManager.remove_food_pollo(1)
+	else: print("No tienes para cocinar")
 	
 func _on_noddle_button_pressed() -> void:
-	_spawn_minigame(minijuego[1])
+	if GameManager.maruchan_cantidad > 0:
+		_spawn_minigame(minijuego[1])
+		GameManager.remove_food_maruchan(1)
+	else: print("No tienes para cocinar")
 	
 func _on_tacos_button_pressed() -> void:
-	_spawn_minigame(minijuego[2])
+	if GameManager.taco_cantidad > 0:
+		_spawn_minigame(minijuego[2])
+		GameManager.remove_food_tacos(1)
+	else: print("No tienes para cocinar")
 
 func _spawn_minigame(mini_game : PackedScene)->void:
 	if hay_juego_activo:
